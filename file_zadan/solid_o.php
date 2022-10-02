137f2ab308bb27bb2edb8a4824f78c01629b2f24
MSW <107107336+MK129y@users.noreply.github.com>
2022-10-02 23:23:46 +0300

    Create solid_o.php

---
+++ file_zadan/solid_o.php
@@ -0,0 +1,19 @@
+class ObjectsItter implements IObjectsItter { 
+    protected $objects = [];
+    protected $handleObjects = [];
+    
+    public function __construct($objects) {
+        $this->objects = $objects;
+    }
+
+    public function handleObjects() {
+        foreach($objects as $object) {
+            $this->handleObjects[] = (object) $object;
+        }
+        return $this->handleObjects;
+    }
+
+}
+
+$soh = new ObjectsItter();
+$soh->handleObjects(array("object_1", "object_2"));
